6d1ec3a43ecda13845765f9aec6f6eb97854e5ca
Jozi-Mwangi <mwasjozi@gmail.com>
2023-10-01 23:16:25 +0300

    Nav configurations

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,36 +1,61 @@
 "use client"
 
 import "dotenv/config"
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import { BsGithub, BsInstagram, BsLinkedin, BsWhatsapp } from "react-icons/bs"
 import { MdMenu } from "react-icons/md"
 import { FaXTwitter } from "react-icons/fa6"
 import { SiGmail } from "react-icons/si"
+import { AiOutlineClose } from "react-icons/ai"
 
 require("dotenv").config();
 
 const NAVIGATION_ITEMS = [
-    {title:"ABOUT"},
-    {title:"CONTACT"},
-    {title:"PROJECTS"}
+    {title:"About"},
+    {title:"Contact"},
+    {title:"Projects"}
 ]
 
 
 const Nav = () => {
+
+    const [nav, setNav] = useState(true);
+
+    const handleNav = ()=> {
+        setNav(!nav)
+    }
+
   return (
     <nav>
-        <div className="flex justify-between px-4 py-2 items-center" >
-            
-            <div className='hidden'><Image src="/profile.png" height={50} className='rounded-full' width={50} alt='profile' /></div>
-            <div className='hidden' ><MdMenu size={40}/></div>
+        <div className="flex flex-col justify-between px-4 py-2 items-center" >
+            <div className="flex mx-auto md:px-24 justify-between w-full items-center" >
+                <div className=''><Image src="/profile.png" height={50} className='rounded-full' width={50} alt='profile' /></div>
 
-            <div className='my-4 flex flex-col items-center w-full text-3xl'>
+                <div onClick={handleNav} className="block md:hidden" >
+                    <div className='right-5' >
+                        { nav? <MdMenu size={40}/>: <AiOutlineClose size={40} />}
+                    </div>    
+                </div>
+            
+                <div className="hidden md:flex text-xl" >
+                {
+                    NAVIGATION_ITEMS.map((item)=>(
+                        <div className='mx-7' >
+                            {item.title}
+                        </div>
+                    ))
+                }    
+                </div>            
+            </div>
+
+
+            <div className={ !nav ?'my-4 flex flex-col items-center w-full text-3xl' : 'hidden'} >
                 {
                     NAVIGATION_ITEMS.map((item)=>(
                         <div className='my-7' >
-                            {item.title}
+                            {item.title.toUpperCase()}
                         </div>
                     ))
                 }
